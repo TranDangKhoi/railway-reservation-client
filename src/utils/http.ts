79a28224bf7c37a690d9a868e021719dc8d625de
TranDangKhoi <baits16@gmail.com>
2023-03-13 00:11:48 +0700

    :100: Create http class for re-useable api call

---
+++ src/utils/http.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosInstance } from "axios";
+
+class Http {
+  instance: AxiosInstance;
+  constructor() {
+    this.instance = axios.create({
+      baseURL: "https://localhost:7098/api",
+      timeout: 10000,
+    });
+  }
+}
+
+const http = new Http().instance;
+
+export default http;
