5cabfd9649b925013748244ae2431f3185fd2987
TranDangKhoi <baits16@gmail.com>
2023-03-13 19:04:28 +0700

    :nail_care: Imrpove UI/UX

--- src/layouts/AuthenticationLayout/AuthenticationLayout.tsx
+++ src/layouts/AuthenticationLayout/AuthenticationLayout.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import React from "react";
 import { useMatch } from "react-router-dom";
 import AuthenticationNavbar from "src/components/AuthenticationNavbar";
@@ -14,19 +13,6 @@ const AuthenticationLayout = ({ children }: AuthenticationLayoutProps) => {
       <AuthenticationNavbar></AuthenticationNavbar>
       <div className="relative h-full bg-randomBackground bg-center lg:h-[650px]">
         <div className="container bg-left-center lg:bg-logoBackground lg:bg-no-repeat">{children}</div>
-        <h2
-          className={classNames(
-            "absolute  text-6xl font-bold",
-            {
-              "top-[410px] left-64": isMatchLogin,
-            },
-            {
-              "top-[460px] left-64": !isMatchLogin,
-            },
-          )}
-        >
-          Railway
-        </h2>
       </div>
     </div>
   );
