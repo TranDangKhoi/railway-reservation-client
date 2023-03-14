88f9320cb081ac4c1f44e3e4bd41f8097aa0c101
TranDangKhoi <baits16@gmail.com>
2023-03-14 16:54:34 +0700

    :+1: Add some more tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,25 +9,31 @@ module.exports = {
   },
   theme: {
     extend: {
+      screens: {
+        xs: "365px",
+      },
       colors: {
         primary: "#3B71FE",
         hover: "#1E5AF4",
         disabled: "#E5E5E5",
         input: "#E7ECF3",
+        primaryGray: "#F4F5F6",
+        secondaryGray: "#84878B",
       },
       fontFamily: {
-        primary: ["DM Sans", "Roboto", "sans-serif"],
-        secondary: ["Roboto", "DM Sans", "sans-serif"],
+        primary: ["Poppins", "DM Sans", "Roboto", "sans-serif"],
+        secondary: ["Roboto", "Poppins", "DM Sans", "sans-serif"],
+        tertiary: ["DM Sans", "Poppins", "DM Sans", "sans-serif"],
       },
       backgroundImage: {
         homepageBackground: "url('./src/assets/images/TrainStation2.png')",
-        logoBackground: "url('./src/assets/images/BigWhiteLogo.png')",
+        logoBackground: "url('./src/assets/images/BigWhiteLogo2.png')",
         randomBackground:
           "url('https://images.unsplash.com/photo-1543746379-597ec1921f02?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80')",
       },
       backgroundPosition: {
         "bottom-center-4": "60% 65%",
-        "left-center": "0% 45%",
+        "left-center": "0% 150px",
       },
     },
   },
