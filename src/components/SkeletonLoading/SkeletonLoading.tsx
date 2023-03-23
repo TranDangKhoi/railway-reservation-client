d3f6183cf576ef3849c8149a0a3efc9cb5e6fcc6
TranDangKhoi <baits16@gmail.com>
2023-03-23 18:56:11 +0700

    💅 Add skeleton loading for UI/UX improvements

---
+++ src/components/SkeletonLoading/SkeletonLoading.tsx
@@ -0,0 +1,40 @@
+const SkeletonLoading = () => {
+  return (
+    <>
+      <div
+        role="status"
+        className="mt-32 max-w-full animate-pulse space-y-4 divide-y divide-gray-200 rounded border border-gray-200 p-4 shadow dark:divide-gray-700 dark:border-gray-700 md:p-6"
+      >
+        <div className="flex items-center justify-between">
+          <div>
+            <div className="my-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+            <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+            <div className="my-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+            <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+          </div>
+          <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+        </div>
+        <div className="flex items-center justify-between">
+          <div>
+            <div className="my-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+            <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+            <div className="my-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+            <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+          </div>
+          <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+        </div>
+        <div className="flex items-center justify-between">
+          <div>
+            <div className="my-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+            <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+            <div className="my-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+            <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+          </div>
+          <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SkeletonLoading;
