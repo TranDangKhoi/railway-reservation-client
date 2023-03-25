228963f5c170a1924d335ff529c80334ec378c45
TranDangKhoi <baits16@gmail.com>
2023-03-26 01:01:51 +0700

    😲 Declare types for tracks

--- src/types/track.types.ts
+++ src/types/track.types.ts
@@ -1,60 +1,33 @@
-const track = {
-  id: 14,
-  departureStation: "Hồ Chí Minh",
-  departureTime: "2023-03-23T00:00:00",
-  arrivalStation: "Hà Nội",
-  returnTime: "2023-03-25T00:00:00",
-  trainTracks: [
-    {
-      id: 4,
-      trackId: 14,
-      trainId: 4,
-      train: {
-        id: 4,
-        name: "SE7",
-        carriages: [
-          {
-            id: 2,
-            carriageNo: 1,
-            totalSeats: 36,
-            trainId: 4,
-          },
-          {
-            id: 2,
-            carriageNo: 1,
-            totalSeats: 36,
-            trainId: 4,
-          },
-        ],
-        totalCarriages: 0,
-      },
-    },
-  ],
-};
-
 export type TrackType = {
+  id: number;
   departureStation: string;
   departureTime: string;
   arrivalStation: string;
+  arrivalTime: string;
   returnTime: string;
-  trainTracks: {
+  trainId: number;
+  train: {
     id: number;
-    trackId: number;
-    trainId: number;
-    train: {
+    name: string;
+    trainCarriages: {
       id: number;
-      name: string;
-      carriages: {
+      carriageNo: number;
+      seats: {
         id: number;
-        carriageNo: number;
-        totalSeats: number;
-        trainId: number;
+        seatPrice: number;
+        seatStatus: number;
+        carriageId: number;
       }[];
-      totalCarriages: number;
-    };
-  }[];
-};
-
-export type TrackListType = {
-  trackList: TrackType[];
+      carriageTypeId: number;
+      carriageType: {
+        id: number;
+        name: string;
+      };
+      totalSeats: number;
+    }[];
+    totalCarriages: number;
+    totalFreeSeats: number;
+    totalReservedSeats: number;
+    totalSeats: number;
+  };
 };
