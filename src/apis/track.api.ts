e517cff331f52d336fc120c4f0ef75018b20681a
TranDangKhoi <baits16@gmail.com>
2023-03-26 01:00:20 +0700

    :heart: Declare types for findTrack api

--- src/apis/track.api.ts
+++ src/apis/track.api.ts
@@ -1,9 +1,10 @@
 import { QueryConfigType } from "src/types/query.types";
+import { TrackType } from "src/types/track.types";
 import http from "src/utils/http";
 
 const trackApi = {
   findTracks: (params: QueryConfigType) =>
-    http.get(`/track/find`, {
+    http.get<TrackType[]>(`/track/find`, {
       params,
     }),
 };
