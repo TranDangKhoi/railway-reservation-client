fbfbe25fdacc18ecc56b375097922c46655ae8b7
TranDangKhoi <baits16@gmail.com>
2023-04-03 01:29:29 +0700

    :+1: Add APIs for creating orders and payments

---
+++ src/apis/order.api.ts
@@ -0,0 +1,10 @@
+import { OrderType } from "src/types/order.types";
+import { SuccessApiResponseType } from "src/types/response.types";
+import http from "src/utils/http";
+
+const orderApi = {
+  getAllOrders: () => http.get<SuccessApiResponseType<OrderType[]>>("/order"),
+  placeOrder: (body: OrderType) => http.post("/order", body),
+};
+
+export default orderApi;
--- src/apis/payment.api.ts
+++ src/apis/payment.api.ts
@@ -1,2 +1,17 @@
-const paymentApi = {};
+import { PaymentType } from "src/types/payment.type";
+import { SuccessApiResponseType } from "src/types/response.types";
+import http from "src/utils/http";
+
+const paymentApi = {
+  makePayment: (userId: string) =>
+    http.post<SuccessApiResponseType<PaymentType>>(
+      "/payment",
+      {},
+      {
+        params: {
+          userId,
+        },
+      },
+    ),
+};
 export default paymentApi;
