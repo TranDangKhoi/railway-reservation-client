dfcc17e0e7b23b710bd4de35e77e899f55b78890
TranDangKhoi <baits16@gmail.com>
2023-04-03 18:08:38 +0700

    :+1: Update api

--- src/apis/order.api.ts
+++ src/apis/order.api.ts
@@ -4,7 +4,12 @@ import http from "src/utils/http";
 
 const orderApi = {
   getAllOrders: () => http.get<SuccessApiResponseType<OrderType[]>>("/order"),
-  placeOrder: (body: OrderType) => http.post("/order", body),
+  placeOrder: (body: OrderType) =>
+    http.post("/order", body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }),
 };
 
 export default orderApi;
