3d62e1a3933d0324961c9caf0c3e2238767e509b
TranDangKhoi <baits16@gmail.com>
2023-04-07 19:59:35 +0700

    :100: Declare more APIs for orders

--- src/apis/order.api.ts
+++ src/apis/order.api.ts
@@ -1,11 +1,14 @@
-import { OrderHistoryType, OrderType } from "src/types/order.types";
+import { OrderHistoryType, OrderRequestType, OrderType } from "src/types/order.types";
 import { SuccessApiResponseType } from "src/types/response.types";
 import http from "src/utils/http";
 
 const orderApi = {
-  getAllOrders: () => http.get<SuccessApiResponseType<OrderType[]>>("/order"),
-  getAllOrdersByUserId: (userId: string) => http.get<SuccessApiResponseType<OrderHistoryType[]>>(`order/${userId}`),
-  placeOrder: (body: OrderType) =>
+  getAllOrders: () => http.get<SuccessApiResponseType<OrderRequestType[]>>("/order"),
+  getAllOrdersByUserId: (userId: string) =>
+    http.get<SuccessApiResponseType<OrderHistoryType[]>>(`order/by-user-id/${userId}`),
+  getOrderDetailByOrderId: (orderId: number) =>
+    http.get<SuccessApiResponseType<OrderHistoryType>>(`order/by-order-id/${orderId}`),
+  placeOrder: (body: OrderRequestType) =>
     http.post("/order", body, {
       headers: {
         "Content-Type": "application/json",
