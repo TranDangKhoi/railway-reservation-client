7d5fda98f17e418f3f8fe368b90372aaa3100146
TranDangKhoi <baits16@gmail.com>
2023-04-07 20:01:12 +0700

    :+1: Update types

--- src/types/order.types.ts
+++ src/types/order.types.ts
@@ -1,6 +1,6 @@
 import { ApplicationUserType } from "./user.types";
 
-export type OrderType = {
+export type OrderRequestType = {
   pickupName: string;
   pickupPhoneNumber: string;
   pickupEmail: string;
@@ -9,12 +9,64 @@ export type OrderType = {
   stripePaymentIntentID: string;
   status: string;
   totalItems: number;
-  orderDetailsDTO: OrderDetailType[];
+  orderDetailsDTO: OrderDetailsDtoType[];
 };
-export type OrderDetailType = {
+
+export type OrderDetailsDtoType = {
   seatId: number;
   price: number;
 };
+
+export type OrderType = {
+  orderHeaderId: number;
+  pickupName: string;
+  pickupPhoneNumber: string;
+  pickupEmail: string;
+  applicationUserId: string;
+  orderTotal: number;
+  orderDate: string;
+  stripePaymentIntentID: string;
+  status: string;
+  totalItems: number;
+  orderDetails: OrderDetailType[];
+};
+
+export type OrderDetailType = {
+  orderDetailId: number;
+  orderHeaderId: number;
+  seatId: number;
+  seat: {
+    id: number;
+    seatNo: number;
+    seatPrice: number;
+    seatStatus: number;
+    carriageId: number;
+    carriage: {
+      carriageNo: number;
+      carriageType: {
+        id: number;
+        name: string;
+      };
+      train: {
+        id: number;
+        name: string;
+        track: {
+          id: number;
+          departureStation: string;
+          departureTime: string;
+          arrivalStation: string;
+          arrivalTime: string;
+        };
+        totalCarriages: number;
+        totalFreeSeats: number;
+        totalReservedSeats: number;
+        totalSeats: number;
+      };
+    };
+  };
+  price: number;
+};
+
 export type OrderHistoryType = {
   orderHeaderId: number;
   pickupName: string;
@@ -27,39 +79,5 @@ export type OrderHistoryType = {
   stripePaymentIntentID: string;
   status: string;
   totalItems: number;
-  orderDetails: {
-    orderDetailId: 15;
-    orderHeaderId: 9;
-    seatId: 111;
-    seat: {
-      id: number;
-      seatNo: number;
-      seatPrice: number;
-      seatStatus: number;
-      carriageId: number;
-      carriage: {
-        carriageNo: number;
-        carriageType: {
-          id: number;
-          name: string;
-        };
-        train: {
-          id: number;
-          name: string;
-          track: {
-            id: number;
-            departureStation: string;
-            departureTime: string;
-            arrivalStation: string;
-            arrivalTime: string;
-          };
-          totalCarriages: number;
-          totalFreeSeats: number;
-          totalReservedSeats: number;
-          totalSeats: number;
-        };
-      };
-    };
-    price: 600000;
-  }[];
+  orderDetails: OrderDetailType[];
 };
