33957d3f8a5f72fb2a6733947e3790035e782bad
TranDangKhoi <baits16@gmail.com>
2023-04-08 22:46:18 +0700

    :+1: Update return types for POST orders API

--- src/apis/cart.api.ts
+++ src/apis/cart.api.ts
@@ -7,7 +7,8 @@ const cartApi = {
     http.get<SuccessApiResponseType<CartType>>("/cart/get-cart", {
       params,
     }),
-  addToCart: (body: { userId: string; seatId: number }) => http.post("/cart/add-to-cart", body),
+  addToCart: (body: { userId: string; seatId: number }) =>
+    http.post<SuccessApiResponseType<CartType>>("/cart/add-to-cart", body),
   removeFromCart: (params: { cartItemId: number; userId: string }) => http.delete("/cart/remove-from-cart", { params }),
 };
 
