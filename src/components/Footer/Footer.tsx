e0b441d40816bf24f9b410ab3f1cd21fc993bbce
TranDangKhoi <baits16@gmail.com>
2023-04-13 23:32:24 +0700

    :+1: Improve translations

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
+import { useTranslation } from "react-i18next";
 import { LogoIcon } from "../Icon";
 
 const Footer = () => {
+  const { t } = useTranslation("footer");
   return (
     <footer className="bg-primaryGray p-5">
       <div className="container">
@@ -15,40 +17,37 @@ const Footer = () => {
                 ></LogoIcon>
                 <span className="text-xl font-bold">Railway</span>
               </div>
-              <span className="hidden text-sm font-medium text-secondaryGray sm:block">
-                Chuẩn bị cho chuyến đi tuyệt vời sắp tới của bạn cùng Railway: Tìm kiếm thông tin hành trình tàu chạy,
-                đặt mua vé tàu hỏa và thanh toán tiện lợi với nhiều cơ hội nhận ưu đãi hấp dẫn.
-              </span>
+              <span className="hidden text-sm font-medium text-secondaryGray sm:block">{t("footer content")}</span>
             </div>
           </div>
           <div className="col-span-1">
             <ul>
               <li>
-                <h3 className="text-xl font-medium leading-[36px]">Dịch vụ</h3>
+                <h3 className="text-xl font-medium leading-[36px]">{t("services")}</h3>
               </li>
-              <li className="mt-1 text-sm font-medium leading-7 text-secondaryGray">Đặt vé tàu</li>
-              <li className="text-sm font-medium leading-7 text-secondaryGray">Tra vé tàu</li>
-              <li className="text-sm font-medium leading-7 text-secondaryGray">Hủy vé tàu</li>
+              <li className="mt-1 text-sm font-medium leading-7 text-secondaryGray">{t("booking")}</li>
+              <li className="text-sm font-medium leading-7 text-secondaryGray">{t("ticket check")}</li>
+              <li className="text-sm font-medium leading-7 text-secondaryGray">{t("return tickets")}</li>
             </ul>
           </div>
           <div className="col-span-1">
             <ul>
               <li>
-                <h3 className="text-xl font-medium leading-[36px]">Hỗ trợ</h3>
+                <h3 className="text-xl font-medium leading-[36px]">{t("support")}</h3>
               </li>
-              <li className="mt-1 text-sm font-medium leading-7 text-secondaryGray">Tài khoản</li>
-              <li className="text-sm font-medium leading-7 text-secondaryGray">Hướng dẫn</li>
-              <li className="text-sm font-medium leading-7 text-secondaryGray">Liên hệ</li>
-              <li className="text-sm font-medium leading-7 text-secondaryGray">Chính sách thanh toán</li>
-              <li className="text-sm font-medium leading-7 text-secondaryGray">Câu hỏi thường gặp</li>
+              <li className="mt-1 text-sm font-medium leading-7 text-secondaryGray">{t("account")}</li>
+              <li className="text-sm font-medium leading-7 text-secondaryGray">{t("instruction")}</li>
+              <li className="text-sm font-medium leading-7 text-secondaryGray">{t("contact")}</li>
+              <li className="text-sm font-medium leading-7 text-secondaryGray">{t("payment policy")}</li>
+              <li className="text-sm font-medium leading-7 text-secondaryGray">{t("faq")}</li>
             </ul>
           </div>
           <div className="col-span-1">
             <ul>
               <li>
-                <h3 className="text-xl font-medium leading-[36px]">Chi tiết</h3>
+                <h3 className="text-xl font-medium leading-[36px]">{t("about us")}</h3>
               </li>
-              <li className="mt-1 text-sm font-medium leading-7 text-secondaryGray">Thông tin {"&"} Địa chỉ</li>
+              <li className="mt-1 text-sm font-medium leading-7 text-secondaryGray">{t("info and address")}</li>
               <li className="text-sm font-medium leading-7 text-secondaryGray">Blog</li>
             </ul>
           </div>
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,24 +2,28 @@ import i18n, { use } from "i18next";
 import { initReactI18next } from "react-i18next";
 import HOME_EN from "src/locales/en/homepage.json";
 import NAVBAR_EN from "src/locales/en/navbar.json";
+import FOOTER_EN from "src/locales/en/footer.json";
 import HOME_VI from "src/locales/vi/homepage.json";
 import NAVBAR_VI from "src/locales/vi/navbar.json";
+import FOOTER_VI from "src/locales/vi/footer.json";
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 
-const resources = {
+export const resources = {
   en: {
     homepage: HOME_EN,
     navbar: NAVBAR_EN,
+    footer: FOOTER_EN,
   },
   vi: {
     homepage: HOME_VI,
     navbar: NAVBAR_VI,
+    footer: FOOTER_VI,
   },
-};
+} as const;
 
-const defaultNS = "homepage";
+export const defaultNS = "homepage";
 
 use(initReactI18next).init({
   resources,
